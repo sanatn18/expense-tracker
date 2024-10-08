8941d4bb27fc2596fed832beba68b4e50c24a166
SanatNaik <sanatn2004@gmail.com>
2024-10-08 17:36:24 +0530

    configured redux store with app. created slice file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/store';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
+
---
+++ src/redux/expensesSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const expenseSlice = createSlice({
+    name:'expenses',
+    initialState: [],
+    reducers: {
+        addExpense: (state, action) => {
+            state.push(action.payload)
+        },
+        removeExpense: (state, action) => {
+            return state.filter((expense)=>expense.id !== action.payload)
+        },
+        updateExpense: (state, action) => {
+            const index = state.findIndex((expense)=>expense.id === action.payload.id);
+            if (index !== -1){
+                state[index] = action.payload;
+            }
+        }
+    }
+})
+
+export const { addExpense, removeExpense, updateExpense } = expenseSlices.actions;
+export default expenseSlice.reducer;
